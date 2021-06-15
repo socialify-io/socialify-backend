edf20c80e86be693c8809bf08879a19c64b7c5de
Pengwius <tomekftomekf@gmail.com>
2021-06-15 23:21:35 +0200

    Takie tam pierdoły

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3 as sql
+import hashlib
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-@app.route('/')
+HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+
+@app.route('/', methods=HTTP_METHODS)
 def what_are_you_looking_for():
     return render_template('what_are_you_looking_for.html')
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=HTTP_METHODS)
 def register():
-    json = request.get_json(force=True) 
+    if request.method != 'POST':
+        return render_template('what_are_you_looking_for.html')
+
+    json = request.get_json(force=True)
 
     if json['password'] == json['repeat_password']:
         con = sql.connect('db/users.db')
@@ -19,7 +25,9 @@ def register():
             return '<p>Username is just owned!</p>'
 
         else:
-            cur.execute(f'INSERT INTO users (username, password) VALUES ("{ json["username"] }", "{ json["password"] }")')
+            enc_pass = hashlib.sha256(bytes(json['password'], 'utf-8')).hexdigest()
+
+            cur.execute(f'INSERT INTO users (username, password) VALUES ("{ json["username"] }", "{ enc_pass }")')
             con.commit()
             con.close()
 
@@ -27,7 +35,7 @@ def register():
     else:
         return '<p>Passwords is not same!</p>'
 
-@app.route('/admin')
+@app.route('/admin', methods=HTTP_METHODS)
 def admin():
     return render_template('admin.html')
 
