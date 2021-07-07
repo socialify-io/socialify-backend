cc38f6ca9b2ac4c7bd39cd4816f9bfb7bc9f0854
Pengwius <tomekftomekf@gmail.com>
2021-07-08 01:58:03 +0200

    Next fixes

--- tests/test_login.py
+++ tests/test_login.py
@@ -60,3 +60,44 @@ def test_login(client):
 
 	assert resp.status_code == 200
 	assert json_resp['success'] == True
+
+def test_getkey(client):
+	resp = client.post(
+		f'{route}/getkey'
+	)
+
+	json_resp = json.loads(resp.data.decode('utf8'))
+
+	print(json_resp)
+
+	global key
+
+	key = json_resp['data']['pubKey']
+
+	assert resp.status_code == 200
+	assert json_resp['success'] == True
+
+def test_register(client):
+	password = 'test_pass123'
+
+	pub_key = RSA.importKey(key)
+	enc_pass = encrypt_public_key(password, pub_key)
+
+	payload = {
+		'username': 'TestAccount123',
+		'password': enc_pass.decode('utf8'),
+		'repeat_password': enc_pass.decode('utf8'),
+		'pubKey': key
+	}
+
+	resp = client.post(
+		f'{route}/register',
+		json=payload
+	)
+
+	json_resp = json.loads(resp.data.decode('utf8'))
+
+	print(json_resp)
+
+	assert resp.status_code == 200
+	assert json_resp['success'] == True
