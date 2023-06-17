abc19c492cc52f8c86ddc4258ebfd5f8c43a6e2c
Marioneq 4958 <mq4958@gmail.com>
2023-06-17 18:51:23 +0200

    Add account subrouter

--- src/routers/account_manager/__init__.py
+++ src/routers/account_manager/__init__.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
+from src.routers.account_manager.account_v1 import router as account_v1_router
+
 router: APIRouter = APIRouter(prefix="/account-manager")
+
+router.include_router(account_v1_router)
