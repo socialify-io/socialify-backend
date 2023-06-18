5978a7ff9484d6a182b68d46f3bfc97243e2837b
Marioneq 4958 <mq4958@gmail.com>
2023-06-18 15:48:27 +0200

    Add /account-manager/oauth2/v1 router

--- src/routers/account_manager/__init__.py
+++ src/routers/account_manager/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from src.routers.account_manager.account_v1 import router as account_v1_router
+from src.routers.account_manager.oauth2_v1 import router as oauth2_v1_router
 
 router: APIRouter = APIRouter(prefix="/account-manager")
 
 router.include_router(account_v1_router)
+router.include_router(oauth2_v1_router)
